167bd9e8578dc530d1da31537d02ceb77eed80bb
JeongKyoungChan <seongbeom@gmail.com>
2022-07-14 08:49:46 +0900

    2dDrawLineandPhysics

---
+++ 2dDrawLineandPhysics/Assets/Scripts/LineDrawer.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineDrawer : MonoBehaviour
+{
+    public GameObject linePrefab;
+    public LayerMask cantDrawOverLayer;
+    int cantDrawOverLayerIndex;
+
+    [Space(30f)]
+    public Gradient lineColor;
+    public float linePointsMinDistance;
+    public float lineWidth;
+
+    Line currentLine;
+    Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        cantDrawOverLayerIndex = LayerMask.NameToLayer("CantDrawOver");
+    }
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            BeginDraw();
+        }
+        if (currentLine != null)
+        {
+            Draw();
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            EndDraw();
+        }
+    }
+    void BeginDraw()
+    {
+        currentLine = Instantiate(linePrefab, this.transform).GetComponent<Line>();
+
+        currentLine.UsePhysics(false);
+        currentLine.SetLineColor(lineColor);
+        currentLine.SetPointsMinDistance(linePointsMinDistance);
+        currentLine.SetLineWidth(lineWidth);
+    }
+    void Draw()
+    {
+        Vector2 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.CircleCast(mousePosition, lineWidth / 3f, Vector2.zero, 1f, cantDrawOverLayer);
+
+        if (hit)
+        { 
+            EndDraw();
+        }
+        else
+        {
+            currentLine.AddPoint(mousePosition);
+        }
+    }
+
+    void EndDraw()
+    {
+        if(currentLine != null)
+        { 
+            if(currentLine.pointsCount < 2)
+            {
+                //if line has on point
+                Destroy(currentLine.gameObject);
+            }
+            else
+            {
+                currentLine.gameObject.layer = cantDrawOverLayerIndex;
+                currentLine.UsePhysics(true);
+                currentLine = null;
+            }
+        }
+    }
+}
